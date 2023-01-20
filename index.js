986b65aed0d95012a65dc2b197c9001c366a3bc7
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-01-20 13:11:12 -0400

    upd

--- index.js
+++ index.js
@@ -103,27 +103,6 @@ function validate() {
   return false;
 }
 
-const counterContainer = document.querySelector('.website-counter');
-const resetButton = document.querySelector('#reset');
-let visitCount = localStorage.getItem('page_view');
-
-// Check if page_view entry is present
-if (visitCount) {
-  visitCount = Number(visitCount) + 1;
-  localStorage.setItem('page_view', visitCount);
-} else {
-  visitCount = 1;
-  localStorage.setItem('page_view', 1);
-}
-counterContainer.innerHTML = visitCount;
-
-// Adding onClick event listener
-resetButton.addEventListener('click', () => {
-  visitCount = 1;
-  localStorage.setItem('page_view', 1);
-  counterContainer.innerHTML = visitCount;
-});
-
 modalOpen();
 modalClose();
 validate();
